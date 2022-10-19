86d81054921ad2c1ea91584a5384ec4312067ae0
Marcin Wrembel <marcin.wrembel@interia.pl>
2022-10-19 13:58:08 +0200

    update for customers

--- js/slider.js
+++ js/slider.js
@@ -1,34 +1,80 @@
+const track = document.querySelector('.slider__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.slider__button--right');
+const prevButton = document.querySelector('.slider__button--left');
+const dotsNav = document.querySelector('.slider__nav');
+const dots = Array.from(dotsNav.children);
 
-let slideIndex = 1;
-showSlides(slideIndex);
+const slideWidth = slides[0].getBoundingClientRect().width;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// arrange the slides next to one another
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  // console.log(dots);
-  if (n > slides.length) {slideIndex = 1}  
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
 
-  if (n < 1) {slideIndex = slides.length}
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+};
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-    // console.log(slides[i])
+const hideShowsArrows = (slides, prevButton, nextButton, targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+  } else {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
   }
-
-  for (i = 0; i < dots.length; i++) {
-    console.log(dots[i].className)
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
+// when I click left, move slides to the left
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex =slides.findIndex(slide => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowsArrows(slides, prevButton, nextButton, prevIndex);
+});
+// when I click right, move slides to the right
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex =slides.findIndex(slide => slide === nextSlide);
+
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowsArrows(slides, prevButton, nextButton, nextIndex);
+});
+//when I click the nav indicators, move to that slide
+
+dotsNav.addEventListener('click', e => {
+  //what indicator was clicked on?
+  const targetDot = e.target.closest('svg');
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  hideShowsArrows(slides, prevButton, nextButton, targetIndex);
+});
